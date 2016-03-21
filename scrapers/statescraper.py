61f0de233f8f6ced0322f3b37c182b904ce7dafe
David <xarchaiax@gmail.com>
2016-03-21 16:12:34 -0400

    Starting PA

--- scrapers/statescraper.py
+++ scrapers/statescraper.py
@@ -3,16 +3,13 @@ import requests
 
 from MD.scraper import ScrapeMD
 
-#This needs to be moved to a database (or should it, how much slower would it be?)
 statesites = {'MD' : 'http://emaryland.buyspeed.com/bso/external/publicBids.sdo',
 				'PA' : 'http://google.com',
 }
 
 
 	
-'''SCRAPE MD'''
+'''SCRAPE MARYLAND'''
 loader = ScrapeMD(statesites['MD'])
 loader.StateScrape()
-'''SCRAPE MD'''
-
-'''SCRAPE PA
+'''SCRAPE MARYLAND'''
