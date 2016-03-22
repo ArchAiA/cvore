65bf33afe3f1d4e18fd897abe97236bde5298798
David <xarchaiax@gmail.com>
2016-03-22 15:19:00 -0400

    MD Scraping - Saving to File

--- scrapers/MD/seltest.py
+++ scrapers/MD/seltest.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import datetime
 import os
 
 
@@ -10,14 +11,27 @@ url = 'http://emaryland.buyspeed.com/bso/external/publicBids.sdo'
 browser.get(url)
 '''Initial Setup for Scraping MD State Contracts'''
 
+def SaveToFile(scraped_data, index):
+	index = index
+	to_be_written = scraped_data
+
+	f = open("../../htmloutput/MD/MDPage" + str(index) + " " + str(datetime.datetime.now()), 'w')
+	f.write()
+	f.close()
+
 
 for index in range(14): #!!! HOW DO WE TELL THIS TO END WHEN THERE ARE NO MORE NEW PAGES
 	try:
 		temp = browser.find_element_by_xpath("/html/body/form/table[4]/tbody/tr[4]/td/table/tbody/tr[2]/td/a[" + str(index) + "]").click()
 		#!!!SAVE TO FILE OR BS4 OR BOTH?
+		SaveToFile(browser.page_source, index)
 		browser.forward()
 	except:
 		pass
+
+
+
+
 
 
 
