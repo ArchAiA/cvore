370c1f02f9dba9f84ca17489c9b0e201894bba72
David <xarchaiax@gmail.com>
2016-03-22 16:07:34 -0400

    Fixed Write To File By Converting to UTF8 Before Write

--- scrapers/MD/seltest.py
+++ scrapers/MD/seltest.py
@@ -1,35 +1,36 @@
 from selenium import webdriver
 import datetime
 import os
+import time
 
 
-'''Initial Setup for Scraping MD State Contracts'''
 path_to_chromedriver = '/home/david/Projects/cvore/dependencies/chromedriver'
 browser = webdriver.Chrome(executable_path = path_to_chromedriver)
 
 url = 'http://emaryland.buyspeed.com/bso/external/publicBids.sdo'
 browser.get(url)
-'''Initial Setup for Scraping MD State Contracts'''
 
 def SaveToFile(scraped_data, index):
 	index = index
 	to_be_written = scraped_data
+	print type(to_be_written)
 
-	f = open("../../htmloutput/MD/MDPage" + str(index) + " " + str(datetime.datetime.now()), 'w')
-	f.write()
+	f = open("../../htmloutput/MD/MDPage" + str(index) + "-" + str(datetime.datetime.now()), 'w')
+	f.write(to_be_written.encode('utf8'))
 	f.close()
 
 
-for index in range(14): #!!! HOW DO WE TELL THIS TO END WHEN THERE ARE NO MORE NEW PAGES
-	try:
-		temp = browser.find_element_by_xpath("/html/body/form/table[4]/tbody/tr[4]/td/table/tbody/tr[2]/td/a[" + str(index) + "]").click()
-		#!!!SAVE TO FILE OR BS4 OR BOTH?
-		SaveToFile(browser.page_source, index)
-		browser.forward()
-	except:
-		pass
+def IterateThroughMDPages():
+	for index in range(14): #!!! HOW DO WE TELL THIS TO END WHEN THERE ARE NO MORE NEW PAGES
+		try:
+			browser.find_element_by_xpath("/html/body/form/table[4]/tbody/tr[4]/td/table/tbody/tr[2]/td/a[" + str(index) + "]").click()
+			#!!!SAVE TO FILE OR BS4 OR BOTH?
+			SaveToFile(browser.page_source, index)
+			browser.forward()
+		except:
+			pass
 
-
+IterateThroughMDPages()
 
 
 
