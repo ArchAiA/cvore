76a1cf9b99f8a4b45327f518810ac22740e58871
David <xarchaiax@gmail.com>
2016-03-23 16:11:20 -0400

    Beginning Work on BS4 Scraping Files

--- scrapers/MD/seltest.py
+++ scrapers/MD/seltest.py
@@ -5,28 +5,21 @@ from selenium import webdriver
 
 
 class ScrapeMD(object):
-# url = 'http://emaryland.buyspeed.com/bso/external/publicBids.sdo'
-	# self.url = extra
 
 	def __init__(self, url, browser):
 		self.url = url
 		self.browser = browser
 		self.browser.get(self.url)
 		self.numberOfContracts = self.GetNumberOfContracts()
-		print self.numberOfContracts
-		# self.IterateThroughMDPages()
 
-	def SaveToFile(self, scraped_data, index):
-		to_be_written = scraped_data.encode('utf8')
-
-		f = open("../htmloutput/MD/MDPage" + str(index) + "-" + str(datetime.datetime.now()) + ".html", 'w')
-		f.write(to_be_written)
-		f.close()
-
+	def GetNumberOfContracts(self):
+		MDNumContracts = self.browser.find_element_by_xpath("/html/body/form/table[4]/tbody/tr[4]/td/table/tbody/tr[1]/td")
+		MDNumContracts = MDNumContracts.get_attribute('innerHTML')
+		MDNumContracts = str(MDNumContracts).rsplit(' ', 1)[1]
+		return int(MDNumContracts)
 
 	def IterateThroughMDPages(self):
 		numberOfPages = self.numberOfContracts / 25
-		print numberOfPages
 		for index in range(1,numberOfPages+2): #Why do I have to add the +2 to the number of pages to cover all of the pages.  I would understand +1, but wtf is up with +2
 			try:
 				# self.browser.find_element_by_xpath("/html/body/form/table[4]/tbody/tr[4]/td/table/tbody/tr[2]/td/a[" + str(index) + "]").click()
@@ -36,11 +29,22 @@ class ScrapeMD(object):
 			except:
 				pass
 
-	def GetNumberOfContracts(self):
-		MDNumContracts = self.browser.find_element_by_xpath("/html/body/form/table[4]/tbody/tr[4]/td/table/tbody/tr[1]/td")
-		MDNumContracts = MDNumContracts.get_attribute('innerHTML')
-		MDNumContracts = str(MDNumContracts).rsplit(' ', 1)[1]
-		return int(MDNumContracts)
+	def SaveToFile(self, scraped_data, index):
+		to_be_written = scraped_data.encode('utf8')
+
+		f = open("../htmloutput/MD/MDPage" + str(index) + "-" + str(datetime.datetime.now()) + ".html", 'w')
+		f.write(to_be_written)
+		f.close()
+
+	def ScrapeFiles(self):
+		#Need to scrape <table id="resultsTable" name="resultsTable" class="table-02">
+		#Each row in the table increments the tableStripe class as seen below
+			#<tr class="tableStripe-01"><td class="tableText-01" align="center" valign="top">
+			#<tr class="tableStripe-02"><td class="tableText-01" align="center" valign="top">
+		#I think that each tableStripe has the same number of <td> elements
+
+	def WriteToDatabase(self):
+
 
 
 
